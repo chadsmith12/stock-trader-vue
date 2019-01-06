69956b6b5bce0075bc19621054b791281801e1ca
Chad Smith <chadsmith4@my.unt.edu>
2019-01-06 13:42:52 -0600

    add basic stock(s) component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,16 @@
 <template>
-    <h1>
-        The Stocks-Stock Component
-    </h1>
+<b-col>
+    <b-card header-bg-variant="light" header-text-variant="dark">
+        <div slot="header">
+            NAME
+            <small>(Price: PRICE)</small>
+        </div>
+        <div class="pull-left">
+            <b-form-input type="number" placeholder="Quantity"></b-form-input>
+        </div>
+        <div class="pull-right">
+            <b-button variant="success">Buy</b-button>
+        </div>
+    </b-card>
+</b-col>
 </template>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,5 +1,25 @@
 <template>
-    <h1>
-        The Stocks Component
-    </h1>
+<b-card-group>
+    <app-stock v-for="stock in stocks" v-bind:key="stock.id"></app-stock>
+</b-card-group>
 </template>
+
+<script>
+import Stock from './Stock.vue';
+
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: 'Apple', price: 250 },
+        { id: 2, name: 'Google', price: 110 },
+        { id: 3, name: 'Lexus', price: 50 },
+        { id: 4, name: 'Twitter', price: 8 },
+      ],
+    };
+  },
+  components: {
+    appStock: Stock,
+  },
+};
+</script>
